18a8d1acffc3093545898654451c317a25d3bcb1
Srujan-rai <raisrujan@gmail.com>
2025-05-08 12:43:16 +0530

    admin-testing

--- under_dev/firebase_setup.py
+++
@@ -1,6 +0,0 @@
-# firebase.py
-import firebase_admin
-from firebase_admin import credentials
-
-cred = credentials.Certificate("docnest-f85e2-firebase-adminsdk-fbsvc-4f30712b1b.json")
-firebase_admin.initialize_app(cred)
